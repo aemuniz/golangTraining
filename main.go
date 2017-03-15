cc8875e8a714d323082f0073e38dccd8437569a3
Arthur Muniz <arthurmuniz@MacMiniDeArthur.local>
2017-03-15 01:00:55 -0300

    loops, unicode, character encoding, character formatting, printing formatted strings

--- main.go
+++ main.go
@@ -2,8 +2,25 @@ package main
 
 import "fmt"
 
-//I don't have to worry about imports
+//I don't have to worry about imports // the webstorm ide does it automatically for me!!
 
 func main() {
+
 	fmt.Println("Hello World!!")
+	fmt.Printf("%d - %b \n", 42, 42)
+	fmt.Printf("%d - %b - %x \n", 42, 42, 42)
+	fmt.Printf("%d - %b - %#x \n", 42, 42, 42)
+	fmt.Printf("%d \t %b \t %#X \n", 42, 42, 42)
+
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t %x \n", i, i, i)
+	}
+
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d \t %b \t %#x \t %q \n", i, i, i, i)
+	}
 }
+
+
+
+
